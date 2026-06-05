de5a85fc892654bea587275b60959708fe1f8e05
Aniruddha-12 <payas.sanjay@gmail.com>
2026-06-05 13:54:13 +0530

    fixed the cors and routing problem here

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,16 @@ import { authRouter } from './middleware/routes/auth.js';
 import  { documentRouter } from './middleware/routes/documents.js';
 // import { configDotenv } from 'dotenv';
 
+const app = express();
+app.use(cors({
+    origin: [
+        'http://localhost:5000', 
+        'http://localhost:3001',
+        process.env.FRONTEND_URL || ''
+    ],
+    credentials: true,
+}));
+
 process.on('unhandledRejection', (reason, promise) => {
     console.error('🚨 [CRITICAL] Unhandled Rejection at:', promise, 'reason:', reason);
     // The server stays alive!
@@ -23,96 +33,12 @@ process.on('uncaughtException', (error) => {
 
 
 
-const app = express();
 const PORT = process.env.PORT || 5000
 
 
-// app.post('/api/register',async(req,res)=>{
-//     try{
-//         const{email,password} = req.body
 
-//         if(!email || !password){
-//             return res.json({error:"Missing email or password"})
-//         }
 
-//         const hashedPassword = await bcrypt.hash(password,10)
 
-//         const sql = `
-//             INSERT INTO users (email, password_hash) 
-//             VALUES ($1, $2) 
-//             RETURNING id, email, created_at;
-//         `
-//         const result = await dbPool.query(sql, [email, hashedPassword])
-
-//         return res.status(201).json({
-//             message: "User registered successfully", 
-//             user: result.rows[0]
-//         })
-//     }catch(error:any){
-//         if(error.code == '23505'){
-//             return res.status(409).json({error:"Email already registered"})
-//         }
-//         console.error(`[Register Error]`,error)
-//         return res.status(500).json({
-//             error:"Internal Server Error"
-//         })
-//     }
-// })
-
-// app.post('/api/login',async(req,res)=>{
-//     try{
-
-    
-//     const {email,password}=req.body
-
-//     if (!email || !password) {
-//             return res.status(400).json({ error: "Missing email or password" });
-//         }
-
-//         const userResult = await dbPool.query(`SELECT * FROM users WHERE email = $1`,[email])
-
-//         if(userResult.rows.length===0){
-//             return res.status(401).json({error:"Invalid Password or Email"})          
-//         }
-
-//         const user = userResult.rows[0]
-
-//         const isMatch = await bcrypt.compare(password, user.password_hash)
-
-//         if(!isMatch){
-//             return res.status(401).json({error:"Invalid Email or Password"})
-//         }
-//         if(!process.env.JWT_SECRET){
-//             throw new Error("JWT_SECRET is missing from environment variables")
-//         }
-
-//         const token = jwt.sign(
-//             {user_id:user.id,email:user.email},
-//             process.env.JWT_SECRET,
-//             {expiresIn:'24h'}
-//         )
-
-//         return res.json({
-//             message:"Login Sucessfull",
-//             token:token
-//         })
-//     }
-//     catch(error:any){
-//         console.error("[LOGIN ERROR]", error);
-//         return res.status(500).json({ error: "Internal Server Error" })
-//     }
-
-        
-// })
-
-app.use(cors({
-    origin: [
-        'http://localhost:5000', 
-        'http://localhost:3001',
-        process.env.FRONTEND_URL || ''
-    ],
-    credentials: true,
-}));
 app.use(express.json())
 
 app.use('/api/chat',chatRouter)
@@ -121,4 +47,6 @@ app.use('/api/documents',documentRouter)
 app.use('/api/auth',authRouter)
 
 
-app.listen(5000, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(Number(PORT), '0.0.0.0', () => {
+    console.log(`🚀 Server seamlessly running on port ${PORT}`);
+})

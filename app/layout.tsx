dbc1efd642340cf1173889c7a97a554538f86faf
Aniruddha-12 <payas.sanjay@gmail.com>
2026-06-30 18:10:56 +0530

    widget ready

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import ServerGuard from '@/components/ServerGuard/ServerGuard';
-import Script from 'next/script';
+
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
